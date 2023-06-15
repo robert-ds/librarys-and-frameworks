705ade60492b346f53e3a47185db9c8b7ae7524d
Robert Vásquez <vasquezrober09@gmail.com>
2023-06-15 14:46:50 -0400

    Test Vue and Svelte

---
+++ svelte/Form.svelte
@@ -0,0 +1,36 @@
+<script>
+  const movies = [
+    {
+      name: "Avengers",
+      quantity: 0,
+      available: 3,
+    },
+    {
+      name: "Terminator",
+      quantity: 0,
+      available: 5,
+    },
+  ];
+</script>
+
+{#each movies as movie}
+  <form>
+    <p>¿Cuántos boletos para {movie.name}?</p>
+
+    <div>
+      <button
+        type="button"
+        on:click={() => movie.quantity--}
+        disabled={movie.quantity <= 0}>-</button
+      >
+
+      <span> {movie.quantity} </span>
+
+      <button
+        type="button"
+        on:click={() => movie.quantity++}
+        disabled={movie.quantity >= 5}>+</button
+      >
+    </div>
+  </form>
+{/each}
